d745d1c460f09aa920af1d115c324810066bca5b
HeyyCzer <43618377+HeyyCzer@users.noreply.github.com>
2023-08-10 19:28:46 -0300

    OpenGraph and favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,13 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
 	title: "SirenTool - The carcols.meta Editor",
 	description: "The simple carcols.meta editor for GTA V/FiveM",
+	openGraph: {
+		siteName: "SirenTool",
+		title: 'SirenTool - The carcols.meta Editor',
+		description: 'The simple carcols.meta editor for GTA V/FiveM',
+		url: 'https://heyyczer.github.io/SirenTool',
+		images: '/siren.png',
+	}
 };
 
 export default function RootLayout({ children }) {
