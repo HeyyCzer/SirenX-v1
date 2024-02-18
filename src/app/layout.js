7acac13c01fc3574ff8328d6d2d410b64aa2f5fc
Rafael <43618377+HeyyCzer@users.noreply.github.com>
2024-02-18 17:56:50 +0000

    Redirect to SirenX v2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,12 @@
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "./globals.css";
 
-import { NavigationEvents } from "@/components/NavigationEvents";
-import * as gtag from "@/lib/gtag";
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import Script from "next/script";
+import { redirect } from "next/navigation";
 config.autoAddCss = false
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
 	title: "SirenX - The carcols.meta Editor",
@@ -24,28 +22,30 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="pt">
-			{/* Global Site Tag (gtag.js) - Google Analytics */}
-			<Script
-				strategy="afterInteractive"
-				src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-			/>
-			<Script id="gtag-init" strategy="afterInteractive">
-				{`
-					window.dataLayer = window.dataLayer || [];
-					function gtag(){dataLayer.push(arguments);}
-					gtag('js', new Date());
+	redirect("https://sirenx.heyyczer.com")
 
-					gtag('config', '${gtag.GA_TRACKING_ID}');
-				`}
-			</Script>
+	// return (
+	// 	<html lang="pt">
+	// 		{/* Global Site Tag (gtag.js) - Google Analytics */}
+	// 		<Script
+	// 			strategy="afterInteractive"
+	// 			src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+	// 		/>
+	// 		<Script id="gtag-init" strategy="afterInteractive">
+	// 			{`
+	// 				window.dataLayer = window.dataLayer || [];
+	// 				function gtag(){dataLayer.push(arguments);}
+	// 				gtag('js', new Date());
 
-			<body className={inter.className}>
-				<NavigationEvents />
+	// 				gtag('config', '${gtag.GA_TRACKING_ID}');
+	// 			`}
+	// 		</Script>
 
-				{children}
-			</body>
-		</html>
-	);
+	// 		<body className={inter.className}>
+	// 			<NavigationEvents />
+
+	// 			{children}
+	// 		</body>
+	// 	</html>
+	// );
 }
